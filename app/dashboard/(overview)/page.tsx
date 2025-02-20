dc8b0ccaa015929f6ab9abcd95666eb82c0471bf
Andravitar <andrei.andraven@gmail.com>
2025-02-20 17:22:34 +0100

    Specifying field errors

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,9 +1,7 @@
-import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 
-import {  fetchCardData } from '@/app/lib/data';
 
 import { Suspense } from 'react';
 import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
@@ -15,7 +13,7 @@ import CardWrapper from '@/app/ui/dashboard/cards';
 export default async function Page() {
   //const revenue = await fetchRevenue();
   //const latestInvoices = await fetchLatestInvoices();
-  const {numberOfCustomers, totalPaidInvoices, totalPendingInvoices, numberOfInvoices} = await fetchCardData();
+  //const {numberOfCustomers, totalPaidInvoices, totalPendingInvoices, numberOfInvoices} = await fetchCardData();
 
   return (
     <main>
